b16cf6abb6c294198f36dcac7cb84d098ffe5a89
Blane Anderson <143014086+HiC-BGOD@users.noreply.github.com>
2023-11-29 21:52:40 -0500

    Complete <3
    bippity

--- FinalProjectGame/Exit1.gd
+++ FinalProjectGame/Exit1.gd
@@ -15,9 +15,11 @@ func _on_body_entered(body):
 	# had help from another professor Francis
 	var current_level = get_tree().current_scene.scene_file_path
 	if body.name == "Player":
-		if current_level == "res://Scenes/Levels/Dungeon_1.tscn":
-			get_tree().change_scene_to_file("res://Scenes/Levels/plains_2.tscn")
-		elif current_level == "res://Scenes/Levels/plains_2.tscn":
-			get_tree().change_scene_to_file("res://Scenes/Levels/Dungeon_3.tscn")
-		elif current_level == "res://Scenes/Levels/Dungeon_3.tscn":
-			get_tree().change_scene_to_file("res://Scenes/UI/End_game.tscn")
+		if current_level == "res://game.tscn":
+			get_tree().change_scene_to_file("res://Levels/level2.tscn")
+		elif current_level == "res://Levels/level2.tscn":
+			get_tree().change_scene_to_file("res://Levels/level_3.tscn")
+		elif current_level == "res://Levels/level_3.tscn":
+			get_tree().change_scene_to_file("res://Levels/fin.tscn")
+		elif current_level == "res://Levels/fin.tscn":
+			get_tree().change_scene_to_file("res://main menu/end.tscn")
